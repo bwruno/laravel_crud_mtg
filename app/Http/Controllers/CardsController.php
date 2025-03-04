b5800410c31a4d504ba4717aed174a2b2aad7c47
bwruno <brunoelcobra@gmail.com>
2025-03-04 01:08:00 +0100

    Creacion de cartas terminada

--- app/Http/Controllers/CardsController.php
+++ app/Http/Controllers/CardsController.php
@@ -2,45 +2,81 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Card;
 use Illuminate\Http\Request;
 
 class CardsController extends Controller
 {
-    /** Display a listing of the resource. */
+    /** Menu principal */
     public function index()
     {
-        $cards = Cards::all();
-        return view('cards.index', compact('cards'));
+        $card = Card::all();
+        return view('cards.index', compact('card'));
     }
 
-    /** Show the form for creating a new resource. */
+    /** Redirige a la página para crear una nueva carta */
     public function create()
     {
-        //
+        return view('cards.create');
     }
 
-    /** Store a newly created resource in storage. */
+    /** Recibe los datos de la carta y los guarda en la base de datos */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required',
+            'type' => 'required',
+            'color' => 'required',
+            'rarity' => 'required',
+            'mana_cost' => 'required|numeric',
+            'description' => 'required',
+            'expansion' => 'required',
+        ]);
+        $card = new Card();
+            $card->name = $request->name;
+            $card->type = $request->type;
+            $card->color = $request->color;
+            $card->rarity = $request->rarity;
+            $card->mana_cost = $request->mana_cost;
+            $card->description = $request->description;
+            $card->expansion = $request->expansion;
+        $card->save();
+        return redirect()->route('cards.index')->with('success', 'Carta creada correctamente');
     }
 
-    /** Display the specified resource. */
-    public function show(string $id)
+    /** Redirige a la página para mostrar una carta */
+    public function show(Card $card)
     {
-        //
+        return view('cards.show', compact('card'));
     }
 
-    /** Show the form for editing the specified resource. */
-    public function edit(string $id)
+    /** Redirige a la página para editar una carta */
+    public function edit(Card $card)
     {
-        //
+        return view('cards.edit', compact('card'));
     }
 
-    /** Update the specified resource in storage. */
-    public function update(Request $request, string $id)
+    /** Recibe los datos de la carta y los actualiza en la base de datos */
+    public function update(Request $request, Card $card)
     {
-        //
+        $request->validate([
+            'name' => 'required',
+            'type' => 'required',
+            'color' => 'required',
+            'rarity' => 'required',
+            'mana_cost' => 'required|numeric',
+            'description' => 'required',
+            'expansion' => 'required',
+        ]);
+        $card->name = $request->name;
+        $card->type = $request->type;
+        $card->color = $request->color;
+        $card->rarity = $request->rarity;
+        $card->mana_cost = $request->mana_cost;
+        $card->description = $request->description;
+        $card->expansion = $request->expansion;
+        $card->save();
+        return redirect()->route('cards.index')->with('success', 'Carta actualizada correctamente');
     }
 
     /** Remove the specified resource from storage. */
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CardsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -7,3 +8,10 @@ Route::get('/', function () {
 });
 
 Route::get('/cards', [CardsController::class, 'index'])->name('cards.index');
+Route::get('/cards/create', [CardsController::class, 'create'])->name('cards.create');
+Route::post('/cards', [CardsController::class, 'store'])->name('cards.store');
+Route::get('/cards/{id}', [CardsController::class, 'show'])->name('cards.show');
+Route::get('/cards/{id}/edit', [CardsController::class, 'edit'])->name('cards.edit');
+Route::patch('/cards/{id}/update', [CardsController::class, 'update'])->name('cards.update');
+
+Route::delete('/cards/{id}/destroy', [CardsController::class, 'destroy'])->name('cards.destroy');
