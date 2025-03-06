1ed627138fb55c37e50a88c8a9fb69a536973c47
bwruno <brunoelcobra@gmail.com>
2025-03-06 21:22:46 +0100

    New BaseData Table Created 'Expansiones'

--- app/Models/Expansiones.php
+++ app/Models/Expansiones.php
@@ -6,10 +6,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 
-class Card extends Model
+
+class Expansiones extends Model
 {
     //
-
     use HasFactory, Notifiable;
+
 
 }
